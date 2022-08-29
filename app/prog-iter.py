4920be00366995bd3f6f0759743d57a72a6f0b95
FrancoMartinCuello <c.francomartin@gmail.com>
2022-08-28 21:35:33 -0300

    se creó el programa de iteracion

--- app/prog-iter.py
+++ app/prog-iter.py
@@ -1,3 +1,14 @@
-for x in range(10):
-    print("Iteración: ",x)
-    
+import datetime
+fecha = datetime.datetime.now()
+
+print ('Hoy', 'es',fecha.strftime('%d/%m/%Y'))
+
+import socket
+host = socket.gethostname()
+
+print('Mi nombre de host es %s' %host)
+
+print ("voy a Iterar 10 veces")
+for i in range(10):
+    print ("Iteracion:",i)
+print ("fin")
